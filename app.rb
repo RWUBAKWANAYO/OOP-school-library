69f61880eec781cf02865450d5e0b95c2b6def8e
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-07-21 11:08:09 +0200

    List all books.

--- app.rb
+++ app.rb
@@ -3,3 +3,11 @@ require('./student')
 require('./teacher')
 require('./rental')
 
+def list_all_books(books)
+  if books.empty?
+    puts 'There is no book!'
+  else
+    books.each { |book| puts "Title: '#{book.title}', Author: #{book.author}" }
+  end
+end
+

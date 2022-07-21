3846c7402b5551837346c5c597099288af08e160
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-07-21 11:12:56 +0200

    Create a rental.

--- app.rb
+++ app.rb
@@ -50,3 +50,22 @@ def create_book(books)
   puts 'Book created successfully.'
 end
 
+def create_rental(books, people, rentals)
+  puts 'Select a book from the following list by number'
+  books.map.with_index { |book, index| puts "#{index}) Title: '#{book.title}', Author: #{book.author}" }
+  selected_book = gets.chomp.to_i
+
+  puts 'Select a person from the following list by number (Not ID): '
+  people.map.with_index do |person, index|
+    puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+  end
+
+  selected_person = gets.chomp.to_i
+
+  print 'Date: '
+  provided_date = gets.chomp
+
+  rentals.push(Rental.new(provided_date, people[selected_person], books[selected_book]))
+  puts 'Rental created successfully.'
+end
+

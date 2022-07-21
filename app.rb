568646d348462c3d3f8d68782a5921c4d426e619
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-07-21 11:10:07 +0200

    Create a person.

--- app.rb
+++ app.rb
@@ -19,3 +19,25 @@ def list_all_people(people)
   end
 end
 
+def create_person(people)
+  print 'Do you want to create a student (1) or a teacher (2): '
+  selected_person = gets.chomp.to_i
+  print 'Age: '
+  age = gets.chomp.to_i
+  print 'Name: '
+  name = gets.chomp
+  case selected_person
+  when 1
+    print 'Has parent permission? [Y/N]: '
+    provided_permission = gets.chomp.capitalize
+    student_permission = true if provided_permission == 'Y'
+    student_permission = false if provided_permission == 'N'
+    people.push(Student.new(nil, age, name, parent_permission: student_permission))
+  when 2
+    print 'Specialization: '
+    specialization = gets.chomp
+    people.push(Teacher.new(specialization, age, name))
+  end
+  puts 'Person created successfully.'
+end
+

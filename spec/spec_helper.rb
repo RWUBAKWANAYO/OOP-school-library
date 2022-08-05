3c00d32dadfe6790d7d4feed81fe71b9b2ffad28
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-08-05 18:13:24 +0200

    add helper file that hold all required files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require_relative '../book'
+require_relative '../student'
+require_relative '../classroom'
+require_relative '../teacher'
+require_relative '../person'
+require_relative '../rental'
+require_relative '../capitalize_decorator'
+require_relative '../trimmer_decorator'
